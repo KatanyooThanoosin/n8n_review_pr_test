3297036aaf7d161a1764772ae2573f13ecfbf374
Katanyoo Thanoosin <nongjupz@MacBook-Air-khxng-Katanyoo.local>
2025-06-06 16:52:55 +0700

    test from n8n

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/test', (req, res) => {
-  res.send('n8n test kub pom');
+  res.send('test from n8n kub eiei');
 });
 
 module.exports = router;

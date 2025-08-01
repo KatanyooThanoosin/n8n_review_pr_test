09a74fa02351a5ce7f5bacda6af34d904a4cc52a
Katanyoo Thanoosin <nongjupz@MacBook-Air-khxng-Katanyoo.local>
2025-08-01 14:27:02 +0700

    test from n8n

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/test', (req, res) => {
-  res.send('from auto push code');
+  res.send('from auto push code 2');
 });
 
 module.exports = router;
